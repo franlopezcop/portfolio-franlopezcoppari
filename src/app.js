1625d963aa6aeb98ead73158546bdab585e391c2
franlopezcopÂ <franlopcopp@gmail.com>
2023-02-06 11:05:09 -0300

    css listo hasta las cards

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000;
+
+let publicPath = path.resolve(__dirname, "../public")
+app.use(express.static(publicPath))
+app.set('views', path.resolve(__dirname, 'views')); 
+app.set('view engine', 'ejs')
+
+const mainRouter = require('./routes/mainRouter.js')
+app.use('/', mainRouter)
+// app.get("/",(req,res) => res.sendFile(path.join(viewsPath,"./home.html")))
+
+app.get("/",(req,res) => res.sendFile(path.join(viewsPath,"./home.html")))
+
+
+app.listen(port, () => {
+    console.log("Servidor corriendo en el puerto " + port);
+})
---
+++ src/controllers/mainController.js
@@ -0,0 +1,24 @@
+const mainController = {
+    home:(req, res)=>{
+        const skills = [
+            {name: 'Node.js', img:'/images/node-js.png'},
+            {name: 'React.js', img:'/images/react.png'},
+            {name: 'JavaScript', img:'/images/js.png'},
+            {name: 'HTML5', img:'/images/html.png'},
+            {name: 'CSS', img:'/images/css-3.png'},
+            {name: 'Express.js', img:'/images/express.png'},
+            {name: 'MySQL', img:'/images/mysql.png'},
+            {name: 'Sequelize', img:'/images/sequelize.svg'},
+            {name: 'API', img:'/images/api.png'},
+            {name: 'Git', img:'/images/git.png'},
+            {name: 'Git Hub', img:'/images/github.png'},
+            {name: 'SCRUM', img:'/images/scrum.png'},
+            {name: 'Postman', img:'/images/postman.svg'},
+            {name: 'MVC', img:'/images/architecture.png'},
+        ]
+        
+        
+        res.render('home',{title:'Portfolio', skills})
+    }
+}
+module.exports = mainController
