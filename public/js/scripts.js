d4413892e32d28f4a3ff19f03ddb02770bc948a7
franlopezcopÂ <franlopcopp@gmail.com>
2023-02-07 10:53:54 -0300

    full responsive

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,12 @@
 const toggleMenuElement = document.getElementById('toggle-menu');
 const navListElement = document.getElementById('nav-list');
-// const body = document.getElementsByClassName('nav_item')
+const ref = document.getElementById('ref')
 toggleMenuElement.addEventListener('click',()=>{
     navListElement.classList.toggle('nav-list--show')
 })
 
-// if(navListElement.classList == 'nav-list--show'){
-//     document.addEventListener('click',()=>{
-//         navListElement.classList('nav-list')
-// })
-// }
+if(navListElement.classList == 'nav-list--show'){
+    ref.addEventListener('click',()=>{
+        navListElement.classList('nav-list')
+})
+}
